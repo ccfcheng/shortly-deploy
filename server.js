0bf5b5fefd686125e1842db3968b20060bf59954
Cliff Saporta Cheng <ccfcheng@gmail.com>
2015-07-29 15:31:19 -0700

    Push to Azure

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 var app = require('./server-config.js');
 
-
 var port = process.env.PORT || 4568;
 
 app.listen(port);
@@ -8,3 +7,4 @@ app.listen(port);
 console.log('Server now listening on port ' + port);
 
 /////COMMENT THERE
+
